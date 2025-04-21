98fcb854a6861b9b715ed1100d1d5bc7a7ea0e31
KUJ <thsusekdnlt1@naver.com>
2025-04-21 14:22:45 +0900

    25.04.21 NotepadKnight 프로젝트 최초 생성

---
+++ NotepadKnights/BattleManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotepadKnights
+{
+    internal class BattleManager
+    {
+
+        class MonsterFactory
+        {
+
+        }
+        class Player
+        {
+
+        }
+        class Monster
+        {
+
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# NotepadKnights
-18조 TextRPG 협엽 공간
