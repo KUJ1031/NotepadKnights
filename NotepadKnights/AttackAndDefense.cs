c51d8aa30ed4e0979771e6846e383da7386fc173
KUJ <thsusekdnlt1@naver.com>
2025-04-22 16:30:44 +0900

    2025.04.22 CriticalAndHide 삭제 및  AttackAndDefense 생성
    - CriticalAndHide 삭제 및  AttackAndDefense 생성

--- NotepadKnights/AttackAndDefense.cs
+++ NotepadKnights/AttackAndDefense.cs
@@ -2,24 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System;
 using System.Threading.Tasks;
 
 namespace NotepadKnights
 {
-    //플레이어, 몬스터 치명타or회피 메서드
-
-    public class CriticalAndHide
+    internal class AttackAndDefense
     {
         // 치명타, 회피 확률 변수
         float criticalProbability = 0.15f;
-        float dodgeProbability = 0.15f;
+        float dodgeProbability = 0.1f;
 
         // 치명타, 회피 발생 여부 확인 변수
         public bool onCritical = false;
         public bool onDodge = false;
 
         private Random random = new Random();
+
+        public int Attack(int playerAttack)
+        {
+            CalcCritical();
+            if (onCritical)
+            {
+                Console.WriteLine("치명적인 데미지!");
+                playerAttack *= (int)1.6f;
+            }
+            return playerAttack;
+        }
+        public int Defense(int EnemyAttack, int PlayerDefense)
+        {
+            CalcDodge();
+            if (onDodge)
+            {
+                Console.WriteLine("그러나 아무 일도 일어나지 않았다.");
+            }
+            else
+            {
+                PlayerDefense -= EnemyAttack;
+            }
+            return PlayerDefense;
+        }
 
         public void CalcCritical()
         {
