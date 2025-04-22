cb0cadd1ac1a6ab21a7b12f0e5d2b573d45440fa
Sungju Kim <tjdwn0885@naver.com>
2025-04-22 13:50:42 +0900

    FIX: 클래스 이름 변경 및 수정
    클래스 이름 변경 및 수정

--- NotepadKnights/MainMenuModule.cs
+++ NotepadKnights/MainMenuModule.cs
@@ -97,13 +97,20 @@ namespace NotepadKnights
                 }
             }
         }
-        public int Villiage()
+        public int Villiage(int value)
         {
+            Console.Clear();
             Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
             Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
             Console.WriteLine("1. 상태 보기 / 2. 인벤토리 / 3. 상점 / 4. 전투하기 / 5. 회복하기\n");
             Console.WriteLine("원하시는 행동을 입력해주세요.");
-            return answer;
+            bool isInt = int.TryParse(Console.ReadLine(),out value);
+            if (!isInt)
+            {
+                Console.WriteLine("잘못된 입력입니다. 다시 시도하세요.");
+                return Villiage(value);
+            }
+            return value;
         }
     }
 }
