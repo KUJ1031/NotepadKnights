bc91190a80ce4f1fde2e5b47efebbd79705b61f4
Sungju Kim <tjdwn0885@naver.com>
2025-04-24 16:46:04 +0900

    모듈 에러 수정
    연결부분이 잘못된 부분을 수정했습니다

--- NotepadKnights/MainMenuModule.cs
+++ NotepadKnights/MainMenuModule.cs
@@ -133,7 +133,6 @@ namespace NotepadKnights
 
             Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
             Console.WriteLine("1. 상태 보기 / 2. 인벤토리 / 3. 상점 / 4. 전투하기 / 5. 회복하기 / 6. 퀘스트\n");
-            Console.WriteLine("원하시는 행동을 입력해주세요.");
 
             int choice = InputManager.ReadInt(1, 6);
 
