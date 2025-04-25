d3cedafea23146722f2cff3b6a9e94865b9a10ea
JangHoon Ban <janghoon333@naver.com>
2025-04-25 22:16:28 +0900

    fix : 사용하지 않는 using 삭제
    - 사용하지 않는 using을 삭제했습니다.

--- NotepadKnights/AttackAndDefense.cs
+++ NotepadKnights/AttackAndDefense.cs
@@ -1,13 +1,4 @@
-﻿using NotepadKnights.Monsters;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
-
-namespace NotepadKnights
+﻿namespace NotepadKnights
 {
     public class AttackAndDefense
     {
--- NotepadKnights/Skill.cs
+++ NotepadKnights/Skill.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NotepadKnights;
-
-namespace NotepadKnights
+﻿namespace NotepadKnights
 {
     public class Skill
     {
